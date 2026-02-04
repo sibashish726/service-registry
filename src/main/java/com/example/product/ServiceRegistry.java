22b4a6a5e4d12403258dc1a449ea74628e126b8f
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2026-02-04 16:36:30 +0530

    Added changes for eureka gateway

--- src/main/java/com/example/product/ServiceRegistry.java
+++ src/main/java/com/example/product/ServiceRegistry.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ShoppingCartApplication {
+public class ServiceRegistry {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingCartApplication.class, args);
+		SpringApplication.run(ServiceRegistry.class, args);
 	}
 
 }
